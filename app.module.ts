c7c7b65817e6eeb27ccefc10e6ce6b82cb91755a
abbu1 <30627182+abbu1@users.noreply.github.com>
2018-11-22 16:19:03 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module'; import { RouterModule} from '@angular/router'; 
+import { AppComponent } from './app.component';
+import { TraineeComponent } from './trainee/trainee.component';
+ import {ReactiveFormsModule, FormsModule} from '@angular/forms'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TraineeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,RouterModule.forRoot([
+         {
+            path: 'app-trainee',
+            component: TraineeComponent
+
+         }
+      ])
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
